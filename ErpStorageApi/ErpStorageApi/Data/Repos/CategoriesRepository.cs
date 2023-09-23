83dc09b4f3e15dc01017e7e39dbeedcd5e510f8b
makenchi <lucas.santos9322@gmail.com>
2023-09-23 15:03:20 -0300

    feat: criando repo de categories

---
+++ ErpStorageApi/ErpStorageApi/Data/Repos/CategoriesRepository.cs
@@ -0,0 +1,63 @@
+﻿using ErpStorageApi.Data.Interfaces;
+using ErpStorageApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ErpStorageApi.Data.Repos
+{
+    public class CategoriesRepository : ICategoriesRepository
+    {
+        private readonly DataContext _context;
+        public CategoriesRepository(DataContext context)
+        {
+            _context = context;
+        }
+        public async Task Add(Category category)
+        {
+            try
+            {
+                _context.Categories.Add(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task Delete(Category category)
+        {
+            try
+            {
+                _context.Categories.Remove(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<Category>> GetAll()
+        {
+            return await _context.Categories.ToListAsync();
+        }
+
+        public Task<Category> GetById(int id)
+        {
+            return _context.Categories.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Update(Category category)
+        {
+            try
+            {
+                _context.Categories.Update(category);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
