7a0e9ea55cdef0994a7255f8aa8bb5b02428b10c
makenchi <lucas.santos9322@gmail.com>
2023-09-23 15:31:40 -0300

    feat: injetando dependencias

--- ErpStorageApi/ErpStorageApi/Program.cs
+++ ErpStorageApi/ErpStorageApi/Program.cs
@@ -1,11 +1,22 @@
+using ErpStorageApi.Data;
+using ErpStorageApi.Data.Interfaces;
+using ErpStorageApi.Data.Repos;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
+builder.Services.AddScoped<ICategoriesRepository, CategoriesRepository>();
 
 var app = builder.Build();
 
