600ffeb3fda5e937261c6f4a2ad31e5f8c4ef3b3
makenchi <lucas.santos9322@gmail.com>
2023-09-23 14:39:19 -0300

    feat: criando entidades

---
+++ ErpStorageApi/ErpStorageApi/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace ErpStorageApi.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ProductId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int Quantity { get; set; }
+    }
+}
